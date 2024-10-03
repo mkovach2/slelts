578fe8325bb53d8c69b6f31d0bf7d9aab99c8009
Miles <miles@hyperlightcorp.com>
2024-10-03 15:40:45 -0400

    hmmmm

--- zip_dirs_2.py
+++ zip_dirs_2.py
@@ -35,32 +35,11 @@ def good_dict(dict_in: dict):
     return dict_str
 
 
-def stat_str(
-        path_to,
-        output_file,
-        num_done: int,
-        goodbadstr: str,
-):
-    stab = os.stat(path_to, follow_symlinks = True)
-    # statistic = stab.st_size
-    paf = os.path.abspath(path_to)
-    
-    # out_str = f'{num_done},{goodbadstr},{paf},{statistic}\n'
-    stat_str = ','.join(str(list(stab)).strip(']').strip('[').split(','))
-    out_str = f'{num_done},{goodbadstr},{paf},{hex(stab.st_file_attributes)}\n'
-    
-    with open(output_file, 'a') as yap:
-        yap.write(out_str)
-    
-    return out_str
-
-
 def zip_dirs(
         in_dir: str,
         compresslevel: int = 9, # 0 = minimum compression, 9 = maximum compression
         loud_mode: bool = True,
         logging: bool = True,
-        err_limit: int|None = None
 ):
     in_dir = os.path.abspath(in_dir.strip('\"'))
     
@@ -81,46 +60,24 @@ def zip_dirs(
     if loud_mode:
         print(f'Done getting subdirectories from\n{in_dir}.\n')
     
-    # print(compress_list)
-    
     num_errs = 0
     num_done_here = 0
-    ya = os.path.join(in_dir, "zip_lenf_stat.csv")
-    with open(ya, 'w') as yap:
-        yap.write(
-            'num_done,'+\
-            'goodbadstr,'+\
-            'paf,'+\
-            # 'st_mode,'+\
-            # 'st_ino,'+\
-            # 'st_dev,'+\
-            # 'st_nlink,'+\
-            # 'st_uid,'+\
-            # 'st_gid,'+\
-            # 'st_size,'+\
-            # 'st_atime,'+\
-            # 'st_mtime,'+\
-            # 'st_ctime\n'
-            'attributes\n'
-        )
     for compress_name in compress_list:
         
         subdir_name = os.path.join(os.path.abspath(in_dir), compress_name)
         out_file_path = subdir_name + '.zip'
+        subdir_as_path = pathlib.Path(subdir_name)
+        not_compressed_record = os.path.join(subdir_as_path, "not_compressed.csv")
         
         print(f'\nCompressing {compress_name}...\n')
         
-        
         with zipfile.ZipFile(out_file_path, mode="w") as archive:
-            subdir_as_path = pathlib.Path(subdir_name)
-            
             for pathe in subdir_as_path.rglob("*"):
                 try:
                     print(f"current path = {pathe}")
                     
                     archive_dict = {
                         "filename" : os.path.abspath(pathe),
-                        # "arcname" : "shit50504",
                         "arcname" : os.path.relpath(pathe, start = subdir_as_path),
                         "compress_type" : zipfile.ZIP_DEFLATED,
                         "compresslevel" : compresslevel,
@@ -131,53 +88,53 @@ def zip_dirs(
                 except Exception as exx:
                     num_errs += 1
                     
-                    print("\nBAD one")
-                    print(f"length = {len(os.path.abspath(pathe))}\n")
-                    stat_str(
-                        path_to = pathe,
-                        output_file = ya,
-                        num_done = num_done_here + 1,
-                        goodbadstr = 'bad'
-                    )
-                    
                     if loud_mode:
                         print(exx)
-                    if logging:
-                        with open(os.path.join(in_dir, "zip_dirs_log.txt"), 'a') as log:
-                            
-                            prob_str = f"Problem happened while compressing: ({num_errs})\n"+\
-                                f"{good_dict(archive_dict)}\n"
-                            
-                            if loud_mode:
-                                print(prob_str)
-                            
-                            log.write(prob_str)
-                            
-                            tbo = exx.__traceback__
-                            log.write(f'{exx}: line {tbo.tb_lineno}\n')
-                            for ii in range(4):
-                                log.write(f'{tbo.tb_frame.f_back}\n')
-                                tbo = tbo.tb_next
-                                if tbo is None:
-                                    break
-                            log.write('\n')
-                            if not(err_limit is None):
-                                if num_errs > err_limit:
-                                    break
-                else:
-                    print("~" * 40 + "\ngood one")
-                    print(f"length = {len(os.path.abspath(pathe))}")
-                    print("~" * 40 + "\n")
                     
-                    stat_str(
-                        path_to = pathe,
-                        output_file = ya,
-                        num_done = num_done_here + 1,
-                        goodbadstr = 'GOOD'
-                    )
+                    if os.path.isfile(not_compressed_record):
+                        opentype = 'a'
+                        outstr = '\n'
+                    else:
+                        opentype = 'w'
+                        outstr = "note the following references:\n"+\
+                            "https://docs.python.org/3/library/zipfile.html#zipfile.ZipFile.write,"+\
+                            "for columns 1 thru 4\n"+\
+                            "https://docs.python.org/3/library/os.html#os.stat_result,"+\
+                            "for info about getting the windows file attributes\n"+\
+                            "https://learn.microsoft.com/en-us/windows/win32/fileio/file-attribute-constants,"+\
+                            "for the meanings of different windows file attributes."+\
+                            "specifically of note:\n"+\
+                            "0x 0004 0000,signifies the item \"fully present locally\"\n"+\
+                            "0x 0000 0020,signifies it is \"an archive file or directory\"\n"
+                            
+                        outstr += "win_attr,"
+                        outstr += ','.join(archive_dict.keys()) + '\n'
+                    
+                    print(f"outstr = {outstr}")
+                    
+                    winstat_str = hex(os.stat(archive_dict['filename']).st_file_attributes).split('x')[-1]
+                    if len(winstat_str) < 8:
+                        winstat_str = winstat_str.zfill(8)
+                    winstat_str = f'{winstat_str[:4]} {winstat_str[4:]},'
+                    
+                    outstr += winstat_str
+                    outstr += ','.join(str(list(archive_dict.values())).strip(']').strip('[').split(','))
+                    
+                    with open(not_compressed_record, opentype) as ncr:
+                        ncr.write(outstr.replace('\\\\','/'))
                 
                 num_done_here += 1
-                        
+        
+        if os.path.isfile(not_compressed_record):
+            archive_dict = {
+                "filename" : os.path.abspath(not_compressed_record),
+                "arcname" : os.path.relpath(not_compressed_record, start = subdir_as_path),
+                "compress_type" : zipfile.ZIP_DEFLATED,
+                "compresslevel" : compresslevel,
+            }
+            
+            archive.write(**archive_dict)
+        
         num_done += 1
         
         if loud_mode:
