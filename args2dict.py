32ec52151f63c02d788cd328d321a8b47f0504af
Miles <miles@hyperlightcorp.com>
2025-07-17 14:24:28 -0400

    Update args2dict.py

--- args2dict.py
+++ args2dict.py
@@ -6,6 +6,9 @@ step1 = r'(:[\s\w]+)?\s?=\s?'
 repl1 = r'" : '
 step2 = r'([\n\r]+)\s+'
 repl2 = r'\1"'
+two_enters = r'[\n\r]{2,}'
+
+comment_char = '#'
 
 while again.lower() == 'y':
     
@@ -17,11 +20,11 @@ while again.lower() == 'y':
             line = input("gib texte.  press enter twice to continue.\n")
             # you have to do this bonanzo garbage bc otherwise re thinks 
             # parens and asterisks are a pattern??
-            line = line.replace('(','\(')
-            line = line.replace(')','\)')
-            line = line.replace('*','\*')
+            line = line.replace('(','\\(')
+            line = line.replace(')','\\)')
+            line = line.replace('*','\\*')
             
-            if re.match(line,r'[\n\r]{2,}'):
+            if re.match(line, two_enters):
                 done = 1
             texte += line + '\n'
         
@@ -29,18 +32,32 @@ while again.lower() == 'y':
         texte = texte.split('\n')
         
         for texte_nn in texte:
-            if texte_nn != '':
-                texte_nn = re.sub(step1,repl1,texte_nn,count = 1)
-                # print('\n')
-                texte_nn = re.sub(step2,repl2,texte_nn)
-                texte_nn = texte_nn.replace('\\(','(')
-                texte_nn = texte_nn.replace('\\)',')')
-                texte_nn = texte_nn.replace('\\*','*')
+            texte_nn_split = list(texte_nn.rpartition(comment_char)) # look for comments
+            # print(texte_nn_split)
+            
+            # line_blank = False
+            if texte_nn_split[1] == comment_char or texte_nn_split[-1] != '':
+                if texte_nn_split[0] == '':
+                    tnn = 2
+                else:
+                    tnn = 0
                 
-                if texte_nn[-1] != ',':
-                    texte_nn += ','
+                texte_nn_split[tnn] = texte_nn_split[tnn].rstrip()
                 
-                print(f'\"{texte_nn.strip()}')
+                texte_nn_split[tnn] = re.sub(step1,repl1,texte_nn_split[tnn],count = 1)
+                texte_nn_split[tnn] = re.sub(step2,repl2,texte_nn_split[tnn])
+                
+                texte_nn_split[tnn] = texte_nn_split[tnn].replace('\\(','(')
+                texte_nn_split[tnn] = texte_nn_split[tnn].replace('\\)',')')
+                texte_nn_split[tnn] = texte_nn_split[tnn].replace('\\*','*')
+                
+                texte_nn_split[tnn] = '\"' + texte_nn_split[tnn].strip()
+                
+                if texte_nn_split[tnn][-1] != ',':
+                    texte_nn_split[tnn] += ', '
+                
+                texte_nn = ''.join(texte_nn_split)
+                print(texte_nn)
     
     except Exception as ploop:
         print("FACK! uh oh!  u got:\t\t{}".format(ploop))
@@ -55,3 +72,4 @@ input("\ndone.  press [enter] to close.\n_")
     # repl1 = r'" : '
     # step2 = r'([\n\r]+)\s+'
     # repl2 = r'\1"'
+    # two_enters = r'[\n\r]{2,}'
